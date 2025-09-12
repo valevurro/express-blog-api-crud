d384b13b380a1192547aa96ab5aa7aa2dbc87c07
valevurro <vale.v.00@outlook.it>
2025-09-12 20:01:53 +0200

    Add initial project setup with Express and image assets
    - Created package.json for project configuration and dependencies.
    - Added Express as a dependency.
    - Included multiple image files for use in the application.
    - Created a router file for handling post-related routes.
    - Updated server.js with comments for future implementation details.

--- server.js
+++ server.js
@@ -18,3 +18,4 @@
 //Bonus
 //Implementare un filtro di ricerca nella index che mostri solo i post che hanno un determinato Tag
 //In Show e Destroy, controllare se il parametro si riferisce ad un post esistente, in caso contrario, rispondere con uno stato 404 e un messaggio d’errore, sempre in formato JSON.//
+
